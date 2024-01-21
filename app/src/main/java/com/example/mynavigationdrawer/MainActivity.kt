4f0110c1e75254bdb2ceee3d4a1abd87da2c280e
rafikawardah <rafika.wardah12@gmail.com>
2024-01-21 18:26:37 +0700

    Set Action SnackBar in Floating Icon Message

--- app/src/main/java/com/example/mynavigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/mynavigationdrawer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mynavigationdrawer
 
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -33,8 +34,13 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
+            //SNACKBAR can set action
+            //TOAST only view it
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+                .setAction("Action") {
+                    Toast.makeText(this@MainActivity, "Halo ini action dari snackbar", Toast.LENGTH_SHORT).show()
+                }
+                .show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
